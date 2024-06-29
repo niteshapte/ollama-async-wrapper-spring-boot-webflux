b9274199b0ecc29fa20fd4660e5618a742aba37c
Nitesh Apte <niteshapte@users.noreply.github.com>
2024-06-29 05:57:58 +0200

    Update WebClientCustomConfig.java

--- src/main/java/ollama/async/wrapper/config/WebClientCustomConfig.java
+++ src/main/java/ollama/async/wrapper/config/WebClientCustomConfig.java
@@ -8,9 +8,11 @@ import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import io.netty.channel.ChannelOption;
+import io.netty.handler.logging.LogLevel;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 @Configuration
 public class WebClientCustomConfig {
@@ -22,8 +24,9 @@ public class WebClientCustomConfig {
                 .responseTimeout(Duration.ofSeconds(60))
                 .doOnConnected(conn -> conn
                         .addHandlerLast(new ReadTimeoutHandler(60)) 
-                        .addHandlerLast(new WriteTimeoutHandler(60))); 
-
+                        .addHandlerLast(new WriteTimeoutHandler(60)))
+                .wiretap("reactor.netty.http.client.HttpClient", LogLevel.INFO, AdvancedByteBufFormat.TEXTUAL);
+		
         return WebClient.builder()
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .baseUrl("http://localhost:11434")
