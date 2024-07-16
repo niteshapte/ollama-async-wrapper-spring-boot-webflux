0b2cebb2cb0310bb386c6fa5a73287c0f5e08085
Nitesh Apte <niteshapte@users.noreply.github.com>
2024-07-16 05:27:12 +0200

    Added Javadoc

--- src/main/java/ollama/async/wrapper/app/OllamaAsyncWrapperApplication.java
+++ src/main/java/ollama/async/wrapper/app/OllamaAsyncWrapperApplication.java
@@ -6,12 +6,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
+/**
+ * Main entry point for the Ollama Async Wrapper application.
+ * <p>
+ * This class initializes the Spring Boot application, enabling auto-configuration,
+ * component scanning, and Spring WebFlux to support reactive web application features.
+ */
 @ComponentScan("ollama.async.wrapper*")
 @EnableAutoConfiguration
 @EnableWebFlux
 @SpringBootApplication
 public class OllamaAsyncWrapperApplication {
 
+	/**
+	* Main method to start the Ollama Async Wrapper application.
+	*
+	* @param args Command-line arguments passed to the application.
+	*/
 	public static void main(String[] args) {
 		SpringApplication.run(OllamaAsyncWrapperApplication.class, args);
 	}
