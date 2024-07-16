e027e67abc9f91144debe9222b3ece08f33e97da
Nitesh Apte <niteshapte@users.noreply.github.com>
2024-07-16 05:11:58 +0200

    Update CircuitBreakerCustomConfig.java
    Added Javadoc

--- src/main/java/ollama/async/wrapper/config/CircuitBreakerCustomConfig.java
+++ src/main/java/ollama/async/wrapper/config/CircuitBreakerCustomConfig.java
@@ -8,9 +8,21 @@ import org.springframework.context.annotation.Configuration;
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 
+/**
+ * Configuration class for customizing a CircuitBreaker instance using Resilience4j.
+ * <p>
+ * This configuration defines a CircuitBreaker with custom parameters including failure rate threshold,
+ * wait duration in open state, permitted number of calls in half-open state, and sliding window size.
+ * The CircuitBreaker instance is configured as a Spring bean with the name "ollamaCircuitBreaker".
+ */
 @Configuration
 public class CircuitBreakerCustomConfig {
 
+	/**
+     	* Defines a custom CircuitBreaker bean with specified configuration parameters.
+     	*
+     	* @return CircuitBreaker instance configured with custom parameters.
+     	*/
 	@Bean
 	CircuitBreaker circuitBreaker() {
 		CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
@@ -19,6 +31,7 @@ public class CircuitBreakerCustomConfig {
                 .permittedNumberOfCallsInHalfOpenState(2)
                 .slidingWindowSize(5)
                 .build();
-        return CircuitBreaker.of("ollamaCircuitBreaker", circuitBreakerConfig);
+		
+        	return CircuitBreaker.of("ollamaCircuitBreaker", circuitBreakerConfig);
 	}
 }
