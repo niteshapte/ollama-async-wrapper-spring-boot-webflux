e3d55e452b5052e19654f89dfae433cdfb794271
Nitesh Apte <niteshapte@users.noreply.github.com>
2024-07-16 05:13:01 +0200

    Update RateLimiterCustomConfig.java
    Added Javadoc

--- src/main/java/ollama/async/wrapper/config/RateLimiterCustomConfig.java
+++ src/main/java/ollama/async/wrapper/config/RateLimiterCustomConfig.java
@@ -8,9 +8,21 @@ import org.springframework.context.annotation.Configuration;
 import io.github.resilience4j.ratelimiter.RateLimiter;
 import io.github.resilience4j.ratelimiter.RateLimiterConfig;
 
+/**
+ * Configuration class for customizing a Rate Limiter instance using Resilience4j.
+ * <p>
+ * This configuration defines a Rate Limiter with custom parameters including limit refresh period,
+ * limit for period, and timeout duration. The Rate Limiter instance is configured as a Spring bean
+ * with the name "ollamaRateLimiter".
+ */
 @Configuration
 public class RateLimiterCustomConfig {
 
+	/**
+     	* Defines a custom Rate Limiter bean with specified configuration parameters.
+     	*
+     	* @return RateLimiter instance configured with custom parameters.
+     	*/
 	@Bean
 	RateLimiter rateLimiter() {
 		RateLimiterConfig rateLimiterConfig = RateLimiterConfig.custom()
